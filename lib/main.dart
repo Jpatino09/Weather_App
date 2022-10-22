0d3bffb6f51ac5ffb163db659196f4e2c4fffea3
Juan Estenan Patiño <esteban09code@gmail.com>
2022-10-22 17:25:24 -0500

    Se crea la clase Data services, que aloja la peticion http a la api , y en main se crea un widget para manter en estado de la clase y permita la creacion del provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:weather_application/providers/data_services.dart';
 import 'package:weather_application/screens/cities.dart';
 import 'package:weather_application/screens/data.dart';
 import 'package:weather_application/screens/home.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(AppState());
+
+class AppState extends StatelessWidget {
+  const AppState({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => DataServices(),
+          lazy: false,
+        ),
+      ],
+      child: MyApp(),
+    );
+  }
+}
 
 class MyApp extends StatelessWidget {
   @override
---
+++ lib/providers/data_services.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+//import 'package:weather_application/screens/cities.dart';
+
+class DataServices extends ChangeNotifier {
+  final String _urlSite = 'api.openweathermap.org';
+  final String _segment = '/data/2.5/weather';
+
+  DataServices() {
+    print('Inicialzando servicios');
+
+    getServices_weather('Medellin');
+  }
+
+  // ignore: non_constant_identifier_names
+  void getServices_weather(String city) async {
+    final queryparams = {
+      'q': city,
+      'appid': '5d55d5cbc3e7f16b0f685b815c9f0ec3'
+    };
+
+    var url = Uri.https(_urlSite, _segment, queryparams);
+
+    final response = await http.get(url);
+
+    final Map<String, dynamic> decodedData = json.decode(response.body);
+    print('Llego hasta aqui MK');
+  }
+}
